5093b144e1512920a02d43cb9eaba21a8c9a41d4
Abhay Dixit <dixitshivam534@gmail.com>
2024-08-29 01:40:52 +0530

    update db.json

--- api/server.js
+++ api/server.js
@@ -1,24 +1,24 @@
 // See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
-const fs = require('fs')
-const path = require('path')
+import { create, router as _router, defaults, rewriter } from 'json-server'
+import { readFileSync } from 'fs'
+import { join } from 'path'
 
-const server = jsonServer.create()
+const server = create()
 
 // Allow write operations
-const filePath = path.join('db.json')
-const data = fs.readFileSync(filePath, "utf-8");
+const filePath = join('db.json')
+const data = readFileSync(filePath, "utf-8");
 const db = JSON.parse(data);
-const router = jsonServer.router(db)
+const router = _router(db)
 
 // Comment out to prevent read-only file system error
 // const router = jsonServer.router('db.json')
 
-const middlewares = jsonServer.defaults()
+const middlewares = defaults()
 
 server.use(middlewares)
 // Add this before server.use(router)
-server.use(jsonServer.rewriter({
+server.use(rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
@@ -28,7 +28,7 @@ server.listen(3000, () => {
 })
 
 // Export the Server API
-module.exports = server
+export default server
 
 
 
